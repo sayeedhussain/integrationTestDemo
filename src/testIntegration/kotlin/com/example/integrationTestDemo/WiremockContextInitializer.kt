1c2559b5f7635d995c7f331744398d96b17e23ce
Sayeed <sayeedhussain19@gmail.com>
2021-07-11 17:52:13 +0530

    Add request/response logs to wiremock

--- src/testIntegration/kotlin/com/example/integrationTestDemo/WiremockContextInitializer.kt
+++ src/testIntegration/kotlin/com/example/integrationTestDemo/WiremockContextInitializer.kt
@@ -2,6 +2,9 @@ package com.example.integrationTestDemo
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.http.Request
+import com.github.tomakehurst.wiremock.http.RequestListener
+import com.github.tomakehurst.wiremock.http.Response
 import org.springframework.boot.test.util.TestPropertyValues
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
@@ -14,6 +17,8 @@ class WireMockContextInitializer : ApplicationContextInitializer<ConfigurableApp
         val wireMockServer = WireMockServer(WireMockConfiguration().dynamicPort())
         wireMockServer.start()
 
+        wireMockServer.addMockServiceRequestListener(WireMockRequestListener())
+
         applicationContext.beanFactory.registerSingleton("wireMock", wireMockServer)
 
         applicationContext.addApplicationListener {
@@ -25,5 +30,16 @@ class WireMockContextInitializer : ApplicationContextInitializer<ConfigurableApp
         TestPropertyValues
                 .of("book-service.baseUrl=http://localhost:${wireMockServer.port()}/")
                 .applyTo(applicationContext)
+    }
+
+    class WireMockRequestListener: RequestListener {
+        override fun requestReceived(request: Request?, response: Response?) {
+            println("WireMock request at URL: ${request?.absoluteUrl}")
+            println("WireMock request headers: ${request?.headers}")
+            println("WireMock response statusCode: ${response?.status}")
+            println("WireMock response headers: ${response?.headers}")
+            val responseBody = if (response?.body != null) response?.bodyAsString else "null"
+            println("WireMock response body: $responseBody")
+        }
     }
 }
